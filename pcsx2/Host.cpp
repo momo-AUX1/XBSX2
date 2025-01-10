1a7439fc19d47a8a6500da4cab9522a2d263c331
Stern <stern@sidestore.io>
2025-01-10 15:56:11 -0500

    Misc: Update User Agent to include the Console Model and no longer say PCSX2

--- pcsx2/Host.cpp
+++ pcsx2/Host.cpp
@@ -159,7 +159,7 @@ bool Host::ConfirmFormattedMessage(const std::string_view title, const char* for
 
 std::string Host::GetHTTPUserAgent()
 {
-	return fmt::format("PCSX2 {} ({})", BuildVersion::GitRev, GetOSVersionString());
+	return fmt::format("XBSX2 {} ({})", BuildVersion::GitRev, GetOSVersionString(), GetConsoleModelString());
 }
 
 std::unique_lock<std::mutex> Host::GetSettingsLock()
