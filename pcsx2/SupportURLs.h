a4c19e52900c240e9c44b4bcad9f3d2885f961e4
SternXD <stern@sidestore.io>
2025-01-26 07:29:06 -0500

    header: Update SupportURLs.h header

--- pcsx2/SupportURLs.h
+++ pcsx2/SupportURLs.h
@@ -6,7 +6,7 @@
 #define PCSX2_WEBSITE_URL "https://pcsx2.net/"
 #define PCSX2_FORUMS_URL "https://forums.pcsx2.net/"
 #define PCSX2_GITHUB_URL "https://github.com/PCSX2/pcsx2"
-#define XBSX2_GITHUB_URL "https://github.com/SirMangler/pcsx2"
+#define XBSX2_GITHUB_URL "https://github.com/EmulationCollective/XBSX2"
 #define PCSX2_LICENSE_URL "https://github.com/PCSX2/pcsx2/blob/master/pcsx2/Docs/License.txt"
 #define PCSX2_DISCORD_URL "https://discord.com/invite/TCz3t9k"
 #define EMULATIONCOLLECTIVE_DISCORD_URL "https://discord.gg/emulation-collective-1007582798598647889"
